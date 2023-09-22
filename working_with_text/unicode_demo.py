85a1e167e58ff7193eab7bd7ca1f33f4f6d6b3b9
yaowenqiang <yaowenqiang111@163.com>
2023-09-22 23:19:38 +0800

    add more format demo

--- working_with_text/unicode_demo.py
+++ working_with_text/unicode_demo.py
@@ -38,3 +38,29 @@ ic(len(prefix))
 ic(sys.getsizeof(prefix))
 ic(timeit('text[:-1]', "text='x'*100000"))
 ic(timeit('text.upper()', "text='x'*1000"))
+x = 42
+format(x, 'd') # decimal
+format(x, 'f') # Floating point
+format(x, 's') # String
+format(x, 'e') # Scientific notation
+format(x, 'x') # Hexadecimal
+format(x, 'o') # Octal
+format(x, 'b') # Binary
+format(x, '%') # Percent
+y = 2.71828
+# [width][.precision]code
+format(y, '0.2f')
+# Alignment Modifiers
+# [<|>|^][width][.precision]code
+format(y, '<20.2f') # align left
+format(y, '>20.2f') # align right
+format(y, '^20.2f') # align middle
+# Fill Character
+# [fill][<|>|^][width][.precision]code
+d = 42
+format(d, '08d')
+format(d, '032d')
+format(d, '=^23d')
+
+
+
