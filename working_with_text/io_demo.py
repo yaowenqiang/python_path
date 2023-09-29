f070d54002c6cfd7ff9f68647f96bd567e5ed30b
yaowenqiang <yaowenqiang111@163.com>
2023-09-29 23:38:10 +0800

    add more io demo

--- working_with_text/io_demo.py
+++ working_with_text/io_demo.py
@@ -13,7 +13,9 @@ StringIO
 import codecs
 import sys
 import io
+import glob
 from urllib.request import urlopen
+import base64
 
 from icecream import ic
 ic(open('foo.txt', 'rt'))
@@ -36,10 +38,25 @@ text = u.read()
 ic(text)
 
 
+f = open("jalape\xf1o.txt","a")
+f.write('Bwahahahaha!\n')
+f.close()
 
 u = io.TextIOWrapper(
     urlopen('https://www.python.org'),
-    encoding='utf-8'
-)
+    encoding='utf-8')
 line = u.readline()
 ic(line)
+
+#f = open(b"jalape\xf1o.txt","a")
+files = glob.glob(b'*.txt')
+ic(files)
+files = glob.glob('*.txt')
+for file in files:
+    print(file)
+
+data = b'Hello World'
+base64.b64encode(data)
+ic(base64.b64encode(data))
+s = bytes('jalapeño.txt', 'utf-8')
+ic(base64.b64encode(s))
