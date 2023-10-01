b6c22945dc82a8a2f45f8d5a461a239f5fc66f44
yaowenqiang <yaowenqiang111@163.com>
2023-10-01 23:39:50 +0800

    add decorator for generator demo

--- concurrent/grep.py
+++ concurrent/grep.py
@@ -1,4 +1,13 @@
 from icecream import ic
+def coroutine(func):
+    def start(*args, **kwargs):
+        cr = func(*args, **kwargs)
+        #ic(dir(cr))
+        next(cr)
+        return cr
+    return start
+
+@coroutine
 def grep(pattern):
     ic(f'looking for {pattern}')
     while True:
@@ -6,9 +15,10 @@ def grep(pattern):
         if pattern in line:
             ic(line)
 
-
-
 msg  = 'hello world\n how are you doing today?'
 g = grep('hello')
-g.send(None)
+#g.send(None)
 g.send(msg)
+
+
+
