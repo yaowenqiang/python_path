67c468b65fb1948c128c136062aeecd5e681fb84
yaowenqiang <yaowenqiang111@163.com>
2023-11-01 22:44:07 +0700

    add scalar types demo

---
+++ getting_started/NOTES.md
@@ -0,0 +1,6 @@
+float
+
+IEEE-754 double-precision with 53-bites of binary precision
+
+
+
---
+++ getting_started/factorial.py
@@ -0,0 +1,17 @@
+from math import factorial as fac
+n = 5
+k = 3
+print(fac(n) / fac(k) * fac(n - k))
+print(fac(100))
+print(len(str(fac(n))))
+int_a = 0b10
+int_a = 0b10
+int_a = 0x10
+int_a = 0o10
+int('10000',3)
+
+3e8
+1.616e-35
+float('nan')
+float('inf')
+float('-inf')
