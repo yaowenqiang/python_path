a990a00a171742711d4d876e5cb8845f6b3f1035
yaowenqiang <yaowenqiang111@163.com>
2023-11-18 19:27:02 +0800

    add class decorator factory demo

--- classes_and_object_orientation/location.py
+++ classes_and_object_orientation/location.py
@@ -1,6 +1,47 @@
 #from utility import typename
 from icecream import ic
 import inspect
+import functools
+
+def postcondition(predicate):
+    def function_decorator(f):
+        @functools.wraps(f)
+        def wrapper(self, *args, **kwargs):
+            result = f(self, *args, **kwargs)
+            if not predicate(self):
+                raise RuntimeError(
+                    f'Post-condition {predicate.__name__} not '
+                    f'maintained for {self!r}'
+                )
+            return result
+        return wrapper
+    return function_decorator
+
+def no_duplicates(itinerary):
+    already_seen = set()
+    for location in itinerary._locations:
+        if location in already_seen:
+            return False
+        already_seen.add(location)
+    return True
+
+def invariant(predicate):
+    function_decorator = postcondition(predicate)
+
+    def class_decorator(cls):
+        members = list(vars(cls).items())
+
+        for name, member in members:
+            if inspect.isfunction(member):
+                decorated_member = function_decorator(member)
+                setattr(cls, name, decorated_member)
+        return cls
+
+    return class_decorator
+
+def at_least_two_locations(itinerary):
+    return len(itinerary._locations) >= 2
+
 
 def auto_repr(cls):
     ic(f'Decorating {cls.__name__} with auto_repr')
@@ -71,6 +112,53 @@ class EarthPosition():
     def __repr__(self):
         return f'{type(self)}(latitude={self._latitude}, longitude={self._longitude})'
 
+@auto_repr
+@invariant(no_duplicates)
+@invariant(at_least_two_locations)
+class Iteinerary:
+    @classmethod
+    def from_locations(cls, *locations):
+        return cls(locations)
+
+    #@postcondition(at_least_two_locations)
+    def __init__(self, locations):
+        self._locations = list(locations)
+
+    def __str__(self):
+        return '\n'.join(location.name for location in self._locations)
+
+    @property
+    def locations(self):
+        return tuple(self._locations)
+
+    @property
+    def origin(self):
+        return self._locations[0]
+
+    @property
+    def destination(self):
+        return self._locations[-1]
+
+    #@postcondition(at_least_two_locations)
+    def add(self, location):
+        self._locations.append(location)
+
+    #@postcondition(at_least_two_locations)
+    def remove(self, name):
+        removal_indexes = [
+            index for index, location in enumerate(self._locations)
+            if location.name == name
+        ]
+        for index in reversed(removal_indexes):
+            del self._locations[index]
+
+    #@postcondition(at_least_two_locations)
+    def truncate_at(self, name):
+        stop = None
+        for index, location in enumerate(self._locations):
+            if location.name == name:
+                stop = index + 1
+                self._locations = self._locations[:stop]
 
 if __name__ == '__main__':
     hong_kong = Location('Hong Kong', EarthPosition(22.29, 114.16))
@@ -79,3 +167,20 @@ if __name__ == '__main__':
     rotterdam = Location('Rotterdam', EarthPosition(51.96, 4.47))
     maracaibo = Location('Maracaibo', EarthPosition(10.65, -71.65))
     ic(repr(hong_kong))
+
+    trip = Iteinerary.from_locations(maracaibo, rotterdam, stockholm)
+    ic(trip)
+    ic(str(trip))
+    ic(trip.locations)
+    ic(trip.origin)
+    ic(trip.destination)
+    trip.add(hong_kong)
+    ic(str(trip))
+    trip.remove('Stockholm')
+    ic(str(trip))
+    trip.truncate_at('Rotterdam')
+    ic(str(trip))
+
+    #trip2 = Iteinerary.from_locations(maracaibo)
+    trip2 = Iteinerary.from_locations(maracaibo,stockholm)
+    #trip2.add(stockholm)
