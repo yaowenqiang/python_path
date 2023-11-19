a0e6f12e77856e23e97d421fdf46a64cad079700
yaowenqiang <yaowenqiang111@163.com>
2023-11-19 23:41:06 +0800

    add PreOrderIterator demo

--- implementing_iterators_iterables_and_collections/iterators.py
+++ implementing_iterators_iterables_and_collections/iterators.py
@@ -2,6 +2,7 @@ from icecream import ic
 
 def is_perfect_length(sequence):
     """True if sequence has length 2**n -1, otherwise False."""
+    ic(sequence)
     n = len(sequence)
     return ((n+1) & n == 0) and (n != 0)
 
@@ -29,6 +30,48 @@ class LevelOrderIterator:
     def __iter__(self):
         return self
 
+def _left_child(index):
+    return 2 * index + 1
+
+def _right_child(index):
+    return 2 * index + 2
+
+
+class PreOrderIterator:
+
+    def __init__(self, sequence):
+        if not is_perfect_length(sequence):
+            raise ValueError(
+                f'Sequence of length {len(sequence)} does not represent '
+                'a prefrect binary tree with length 2**n -1'
+            )
+
+        self._sequence = sequence
+        self._stack = [0]
+
+    def __next__(self):
+        if len(self._stack) == 0:
+            raise StopIteration
+
+        index = self._stack.pop()
+        result = self._sequence[index]
+
+        # Pre-order: Push right child first so left child is
+        # popped and processed first, Last-in, first-out
+
+        right_child_index = _right_child(index)
+        if right_child_index < len(self._sequence):
+            self._stack.append(right_child_index)
+
+        left_child_index = _left_child(index)
+        if left_child_index < len(self._sequence):
+            self._stack.append(left_child_index)
+
+        return result
+
+    def __iter__(self):
+        return self
+
 if __name__ == '__main__':
     expr_tree = ['*', '+', '-', 'a', 'b','c','d']
     iterator = LevelOrderIterator(expr_tree)
@@ -42,6 +85,9 @@ if __name__ == '__main__':
     #ic(next(iterator))
     ic(' '.join(iterator))
     ic({i:is_perfect_length(['x']*i) for i in range(0, 32)})
-    ic(LevelOrderIterator('+ 24 12 -'.split()))
+    #ic(LevelOrderIterator('+ 24 12 -'.split()))
+
+    pre_order_iterator = PreOrderIterator(expr_tree)
+    ic(' '.join(pre_order_iterator))
 
 
