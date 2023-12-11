5671e4b9a508b6ae0ac71838c7b3e0af9cc11e59
yaowenqiang <yaowenqiang111@163.com>
2023-12-11 23:55:51 +0800

    add click and sqlite3 demo

--- working_with_database/get_coin.py
+++ working_with_database/get_coin.py
@@ -1,16 +1,39 @@
 import requests
+import sqlite3
 import click
 
-@click.command()
-@click.olption('--coin_id', default='bitcoin')
-@click.olption('--currency', default='usd')
+
+@click.group
+def cli():
+    pass
+
 
 def get_coin_price(coine_id, currency):
     url = f'https://api.coingecko.com/api/v3/simple/prince?{coine_id}&vs_currencies={currency}'
     data = requests.get(url).json()
     coin_price = data[coin_id][currency]
+    return coin_price
+
+@click.command()
+@click.option('--coin_id', default='bitcoin')
+@click.option('--currency', default='usd')
+def show_coin_price(coin_id, currency):
+    coin_price = get_coin_price(coin_id, currency)
     print(f'The price of {coine_id} is {coin_price:.2f} {currency.upper()}')
 
 
+@click.command()
+@click.option('--coin_id', default='bitcoin')
+@click.option('--currency', default='usd')
+@click.option('--amount', type=float)
+@click.option('--sell', is_flag=True)
+
+cli.add_command(show_coin_price)
+
+
+
 if __name__ == '__main__':
-    get_coin_price()
+    database = sqlite3.connect('portfolio.db')
+    cursor = database.cursor()
+    cursor.execute(CREATE_INVESTMENTS_SQL)
+    cli()
