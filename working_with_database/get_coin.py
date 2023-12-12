91fa4ce627d7e233df45a71414bb9b25c27409a5
yaowenqiang <yaowenqiang111@163.com>
2023-12-12 23:31:27 +0800

    add add_investmen sub command

--- working_with_database/get_coin.py
+++ working_with_database/get_coin.py
@@ -1,12 +1,22 @@
 import requests
 import sqlite3
 import click
+import datetime
 
 
 @click.group
 def cli():
     pass
 
+CREATE_INVESTMENTS_SQL = """
+create  table  if not exists investments (
+    coin_id TEXT,
+    currency TEXT,
+    sell INT,
+    amount REAL,
+    date TIMESTAMP
+);
+"""
 
 def get_coin_price(coine_id, currency):
     url = f'https://api.coingecko.com/api/v3/simple/prince?{coine_id}&vs_currencies={currency}'
@@ -27,8 +37,21 @@ def show_coin_price(coin_id, currency):
 @click.option('--currency', default='usd')
 @click.option('--amount', type=float)
 @click.option('--sell', is_flag=True)
+def add_investment(coin_id, currency, amount, sell):
+    sql = 'INSERT into investments VALUES(?,?,?,?,?);'
+    values = (coin_id, currency, amount, sell, datetime.datetime.now())
+    cursor.execute(sql, values)
+    database.commit()
+
+    if sell:
+        print(f'Added sell of {amount} {coin_id}')
+    else:
+        print(f'Added buy of {amount} {coin_id}')
+
+
 
 cli.add_command(show_coin_price)
+cli.add_command(add_investment)
 
 
 
