4cc9ccdc7f41677f93d6fcadd7d8ab5f9f39abcf
yaowenqiang <yaowenqiang111@163.com>
2023-12-12 23:59:05 +0800

    add row factory demo

--- working_with_database/get_coin.py
+++ working_with_database/get_coin.py
@@ -4,7 +4,29 @@ import click
 import datetime
 import random
 import csv
+from dataclasses import dataclass
+import datetime
 
+@dataclass
+class Investment:
+    coin_id: str
+    currency: str
+    amount: float
+    sell: bool
+    date: datetime.datetime
+
+    def compute_value(self) -> float:
+        return self.amount - get_coin_price(self.coin_id, self.currency)
+
+
+def investment_row_factory(_, row):
+    return Investment(
+        coin_id=row[0],
+        currency=row[1],
+        amount=row[2],
+        sell=bool(row[3]),
+        date=datetime.datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S.%f')
+    )
 
 
 @click.group
@@ -41,7 +63,7 @@ def show_coin_price(coin_id, currency):
 def get_investment_value(coin_id, currency):
     coin_price = get_coin_price(coin_id, currency)
     sql = """
-    SELECT amount FROM investments
+    SELECT * FROM investments
     WHERE coin_id=?
     AND currency=?
     AND sell=?;
@@ -49,8 +71,10 @@ def get_investment_value(coin_id, currency):
     buy_result = cursor.execute(sql, (coin_id, currency, False)).fetchall()
     sell_result = cursor.execute(sql, (coin_id, currency, True)).fetchall()
 
-    buy_amount = sum([row[0] for row in buy_result])
-    sell_amount = sum([row[0] for row in sell_result])
+    # buy_amount = sum([row[0] for row in buy_result])
+    # sell_amount = sum([row[0] for row in sell_result])
+    buy_amount = sum([row.amount for row in buy_result])
+    sell_amount = sum([row.amount for row in sell_result])
 
     total = buy_amount - sell_amount
     print(f'You own a total of {total} {coin_id} worth {total * coin_price} {currency.upper()}')
@@ -73,6 +97,7 @@ def add_investment(coin_id, currency, amount, sell):
     else:
         print(f'Added buy of {amount} {coin_id}')
 
+@click.command()
 @click.option('--csv_file')
 def import_investment(csv_file):
     with open(csv_file, 'r') as f:
@@ -97,6 +122,7 @@ cli.add_command(import_investment)
 
 if __name__ == '__main__':
     database = sqlite3.connect('portfolio.db')
+    database.row_factory = investment_row_factory
     cursor = database.cursor()
     cursor.execute(CREATE_INVESTMENTS_SQL)
     cli()
